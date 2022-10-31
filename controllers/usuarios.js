6bf8b699239724f1a93b2d6dd1eab5a6005ddcce
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2022-10-31 17:54:03 -0600

    User & Delete By ID

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,6 +1,62 @@
 const { request, response } = require("express");
-const getUsers = (req = request, res = response) => {
-    console.log("funcion getUser")
-    res.json({msg: "Funcion getUsers"})
+const pool = require("../db/connection");
+const getUsers = async (req = request, res = response) => {
+    let conn;
+    try {
+        conn = await pool.getConnection()
+        const users = await conn.query("SELECT * FROM Usuarios", (error) => {throw new Error(error)})
+        if (!users) {
+            res.status(404).json({msg: "no se encontraron registros"})
+            return
+        }
+        res.json({users})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    } finally {
+        if (conn) {
+            conn.end()
+        }
+    }
 }
-module.exports = {getUsers}
+const getUserByID = async (req = request, res = response) => {
+    const {id} = req.params
+    let conn;
+    try {
+        conn = await pool.getConnection()
+        const [user] = await conn.query(`SELECT * FROM Usuarios WHERE ID = ${id}` , (error) => {throw new Error(error)})
+        if (!user) {
+            res.status(404).json({msg: `no se encontro registro con el ID ${id}`})
+            return
+        }
+        res.json({user})
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({error})
+    } finally {
+        if (conn) {
+            conn.end()
+        }
+    }
+}
+const deleteUserByID = async (req = request, res = response) => {
+    const {id} = req.query
+    let conn;
+    try {
+        conn = await pool.getConnection()
+        const affectedRows = await conn.query(`UPDATE Usuarios SET Activo = 'N' WHERE ID = ${id}` , (error) => {throw new Error(error)})
+        if (affectedRows === 0) {
+            res.status(404).json({msg: `no se pudo eliminar el registro con el ID ${id}`})
+            return
+        }
+    res.json({msg: `el usuario con ID ${id} se elimino correctamente`})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    } finally {
+        if (conn) {
+            conn.end()
+        }
+    }
+}
+module.exports = {getUsers, getUserByID, deleteUserByID}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,11 @@
 const {Router} = require("express")
-const {getUsers}= require("../controllers/usuarios")
+const {getUsers, getUserByID, deleteUserByID}= require("../controllers/usuarios")
 const router =  Router()
 /*  
     En Insomnia poner:
     http://localhost:4000/api/v1/usuarios
 */
 router.get("/", getUsers)
+router.get("/id/:id", getUserByID)
+router.delete("/" , deleteUserByID)
 module.exports = router
