a4d0efb368ae889e02810c87bbe12977ac7204a4
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2022-10-31 15:29:14 -0600

    Installing dotenv

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hola, Mundo!')
 })
-app.listen(4000, () => {
-    console.log("Backend activado =D")
+app.listen(process.env.PORT, () => {
+    console.log("Backend activado en el puerto: ", process.env.PORT)
 })
